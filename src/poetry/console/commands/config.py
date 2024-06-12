9f8074da25750b54a603e0e9242c5ac3da92d078
austinzh <austin.zhang79@gmail.com>
2024-06-12 01:07:50 +0000

    Address comments

--- src/poetry/console/commands/config.py
+++ src/poetry/console/commands/config.py
@@ -77,6 +77,7 @@ To remove a repository (repo is a short alias for repositories):
             "installer.modern-installation": (boolean_validator, boolean_normalizer),
             "installer.parallel": (boolean_validator, boolean_normalizer),
             "installer.max-workers": (lambda val: int(val) > 0, int_normalizer),
+            "installer.max-retries": (lambda val: int(val) >= 0, int_normalizer),
             "installer.no-binary": (
                 PackageFilterPolicy.validator,
                 PackageFilterPolicy.normalize,
